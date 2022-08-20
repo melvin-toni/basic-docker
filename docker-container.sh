ec05e0a98015b1f29b08b6f18a361945be45e755
Melvin Toni <melvin.gustave@sicepat.com>
2022-08-20 13:04:04 +0700

    docker container

---
+++ docker-container.sh
@@ -0,0 +1,17 @@
+docker container ls -a
+
+docker container ls
+
+docker container create --name contohredis redis:latest
+
+docker container start contohredis
+
+docker container start 8fc43d04e610
+
+docker container stop contohredis
+
+docker container stop 8fc43d04e610
+
+docker container rm contohredis
+
+docker container rm 8fc43d04e610
