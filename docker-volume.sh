4418eb0b4cddcd0ff5b6131df6478eb7eb7fa615
Melvin Toni <melvin.gustave@sicepat.com>
2022-08-22 20:13:28 +0700

    mount volume

--- docker-volume.sh
+++ docker-volume.sh
@@ -2,4 +2,12 @@ docker volume ls
 
 docker volume create mongovolume
 
+docker container create --name mongovolumect --publish 27019:27017 --mount "type=volume,source=mongovolume,destination=/data/db" --env MONGO_INITDB_ROOT_USERNAME=melvin --env MONGO_INITDB_ROOT_PASSWORD=melvin mongo:latest
+
+docker container start mongovolumect
+
+docker container stop mongovolumect
+
+docker container rm mongovolumect
+
 docker volume rm mongovolume
