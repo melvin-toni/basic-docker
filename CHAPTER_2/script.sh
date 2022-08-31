e1797c4449205a913daae32af8a7adeff922eef0
Melvin Toni <melvin.gustave@sicepat.com>
2022-08-31 15:43:07 +0700

    dockerfile run command

--- CHAPTER_2/script.sh
+++ CHAPTER_2/script.sh
@@ -1,3 +1,12 @@
+# FROM instruction
 docker build -t melvintoni/from from
 
 docker image ls
+
+# RUN instruction
+docker build -t melvintoni/run run
+
+docker build -t melvintoni/run run --progress=plain --no-cache
+
+## Categorize image by repository name
+docker image ls | grep melvintoni
